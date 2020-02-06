97482df82df6054df36964f30976c3d9e0eb64aa
efla5874 <felipe.lopez.arce@everis.com>
2020-02-06 15:36:40 -0300

    nuestra primera navegacion usando Navigation Components!

---
+++ app/src/main/java/com/mvvm/demo/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.mvvm.demo
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //nextButton.setOnClickListener {
+        //    findNavController().navigate(R.id.action_mainFragment_to_firstFragment)
+        //}
+
+        nextButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_firstFragment))
+    }
+}
